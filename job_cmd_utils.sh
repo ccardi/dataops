5dd9011336a2757b82e57834b338b9250d4d8948
cardi <clement.cardi@gmail.com>
2022-07-28 19:39:20 +0000

    update

--- job_cmd_utils.sh
+++ job_cmd_utils.sh
@@ -3,24 +3,23 @@ git add .
 git commit -m "update"
 git push -u origin master
 
+# Connect to your gke cluster and apply a job
 gcloud container clusters get-credentials autopilot-cluster-2 --region europe-west1 --project pod-fr-retail
 kubectl apply -f job.yaml 
 
-
-cd Jobs/run-k8s-jobs
+# Build and deploy your Cloud Run Service to trigger a job on GKE (test it with curl or pubsub)
+cd run-k8s-jobs
 PROJECT=pod-fr-retail
-gcloud config set project $PROJECT
-
 JOB=run-k8s-jobs
-PROJECT=pod-fr-retail
+gcloud config set project $PROJECT
 gcloud builds submit --tag europe-west1-docker.pkg.dev/$PROJECT/demok8/$JOB
-
 gcloud run deploy $JOB \
 --image=europe-west1-docker.pkg.dev/$PROJECT/demok8/$JOB:latest \
 --platform=managed \
 --region=europe-west1 \
 --project=pod-fr-retail
 
-cd ..
-
+# Trigger your Cloud Run Service or copy run_params.json in your pubsub trigger (see: pubsub-cloud-run-jobs)
 curl -H "Authorization: Bearer $(gcloud auth print-identity-token)" -H "Content-Type: application/json"  -d @run_params.json https://run-k8s-jobs-454dfk63ya-ew.a.run.app
+gcloud pubsub topics publish projects/pod-fr-retail/topics/pubsub-cloud-run-jobs \
+  --message={}
