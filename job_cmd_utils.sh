b94f927fddf945971e9479ba34dc6dc6a02b1317
cardi <clement.cardi@gmail.com>
2022-07-28 19:06:08 +0000

    update

--- job_cmd_utils.sh
+++ job_cmd_utils.sh
@@ -5,3 +5,23 @@ git push -u origin master
 
 gcloud container clusters get-credentials autopilot-cluster-2 --region europe-west1 --project pod-fr-retail
 kubectl apply -f job.yaml 
+
+
+cd Jobs/run-k8s-jobs
+PROJECT=pod-fr-retail
+gcloud config set project $PROJECT
+
+JOB=run-k8s-jobs
+PROJECT=pod-fr-retail
+gcloud builds submit --tag europe-west1-docker.pkg.dev/$PROJECT/demok8/$JOB
+
+gcloud run deploy $JOB \
+--image=europe-west1-docker.pkg.dev/$PROJECT/demok8/$JOB:latest \
+--platform=managed \
+--region=europe-west1 \
+--project=pod-fr-retail
+
+cd ..
+
+curl -H "Authorization: Bearer $(gcloud auth print-identity-token)" -H "Content-Type: application/json"  -d '{"projectId":"pod-fr-retail","region":"europe-west1", "gkeClusterName":"autopilot-cluster-2", "jobStorageBucket":"pod-fr-retail-jobs-conf","jobPathName":"job.yaml", "jobRootName":"pandas-to-bq-"}' https://run-k8s-jobs-454dfk63ya-ew.a.run.app
+{"projectId":"pod-fr-retail","region":"europe-west1", "gkeClusterName":"autopilot-cluster-2", "jobStorageBucket":"pod-fr-retail-jobs-conf","jobPathName":"job.yaml", "jobRootName":"pandas-to-bq-"}
