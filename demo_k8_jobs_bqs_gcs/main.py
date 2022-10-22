c04f25c8cf5bcc8ba87de16bd086a2296377ca3f
clement cardi <clement.cardi@gmail.com>
2022-10-22 19:45:06 +0000

    update

--- demo_k8_jobs_bqs_gcs/main.py
+++ demo_k8_jobs_bqs_gcs/main.py
@@ -2,11 +2,33 @@ from google.cloud import bigquery_storage_v1
 import json
 import csv
 from google.cloud import storage
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--max_rows", help="number_rows", default=20000)
+parser.add_argument("--project_id", help="project_id", required=True)
+parser.add_argument("--dataset", help="dataset", required=True)
+parser.add_argument("--tableId", help="tableId", required=True )
+parser.add_argument("--productsetId", help="productsetId", required=True )
+parser.add_argument("--bucket_name", help="bucket_name", required=True )
+parser.add_argument("--file_prefix", help="file_prefix", required=True )
+parser.add_argument("--selected_fields", help="selected_fields", required=True )
 
-def bqstorage_to_gcs_csv(project_id, productsDataset, productsTable, bucket_name, file_prefix, max_rows, selected_fields):
+args = parser.parse_args()
+
+max_rows=int(args.max_rows)
+project_id=args.project_id
+dataset=args.dataset
+tableId=args.tableId
+selected_fields=args.selected_fields.split(",")
+productsetId=args.productsetId
+bucket_name=args.bucket_name
+file_prefix=args.file_prefix
+
+
+def bqstorage_to_gcs_csv(project_id, dataset, tableId, bucket_name, file_prefix, max_rows, selected_fields):
     print("\nStart extract into {}.{} folder \n".format( bucket_name, file_prefix))
     client_bq = bigquery_storage_v1.BigQueryReadClient()
-    table = "projects/{}/datasets/{}/tables/{}".format( project_id,productsDataset, productsTable)
+    table = "projects/{}/datasets/{}/tables/{}".format( project_id,dataset, tableId)
     requested_session = bigquery_storage_v1.types.ReadSession()
     requested_session.table = table
     requested_session.data_format = bigquery_storage_v1.types.DataFormat.AVRO
@@ -43,3 +65,7 @@ def bqstorage_to_gcs_csv(project_id, productsDataset, productsTable, bucket_name
         i=i+1
         w.writerow(row.values())
     print("\nExtract {} rows into {}.{}[0-{}]  ".format(i, bucket_name, file_prefix, file_suffix_number))
+
+
+
+bqstorage_to_gcs_csv(project_id, dataset, tableId, bucket_name, file_prefix, max_rows, selected_fields)
