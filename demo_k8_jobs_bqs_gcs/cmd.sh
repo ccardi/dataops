fa9d26dbf3e82550cd346e3f83ef1687c18b73a4
clement cardi <clement.cardi@gmail.com>
2022-12-22 15:13:55 +0000

    update

--- demo_k8_jobs_bqs_gcs/cmd.sh
+++ demo_k8_jobs_bqs_gcs/cmd.sh
@@ -6,3 +6,13 @@ python3 main.py \
 --bucket_name='pod-fr-retail-kaggle' \
 --file_prefix='products_vision_search/kaggle_shoes/part-' \
 --selected_fields='image_uri,image_id,product_set_id,product_id,product_category,product_display_name,labels,poly'
+
+
+
+
+# Build k8 Jobs - unzip
+cd demo_k8_jobs_bqs_gcs
+PROJECT=pod-fr-retail
+JOB=demo_k8_jobs_bqs_gcs
+gcloud config set project $PROJECT
+gcloud builds submit --tag europe-west1-docker.pkg.dev/$PROJECT/demok8/$JOB
