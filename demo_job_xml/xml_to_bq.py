77e0c0b6e2c1e4ee445743246d687dba65ebb283
clement cardi <clement.cardi@gmail.com>
2023-09-27 14:05:05 +0000

    update

--- demo_job_xml/xml_to_bq.py
+++ demo_job_xml/xml_to_bq.py
@@ -1,20 +1,19 @@
 import os
-import pandas
-import pandas_gbq
+import pandas as pd
 
 print('================ Start ==================')
 
 project_id = os.environ['PROJECT_ID']
 gcs_xml_path= os.environ['GCS_XML_PATH']
 xpath= os.environ['XML_XPATH']
-namespaces= {"doc":os.environ['XML_XPATH']}
+namespaces= {"doc":os.environ['XML_NAMESPACES']}
 
 destination_table = os.environ['TABLE_ID']
 location='EU'
 
 df = pd.read_xml(
     gcs_xml_path
-    , xpath='.//doc:price-table'
+    , xpath=xpath
     , namespaces=namespaces
 )
 
